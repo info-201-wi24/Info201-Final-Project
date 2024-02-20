fda28172f8d3461d1401055ef76699789a759be4
seanl26 <seanl26@uw.edu>
2024-02-20 14:59:08 -0800

    testing git

--- INFO 201 Final Project.R
+++ INFO 201 Final Project.R
@@ -1,1 +1,1 @@
-obesity.df <- read.csv("https://www.cdc.gov/obesity/data/maps/2022/2022-overall-prevalence.csv")
+obesity.df <- read.csv("https://www.cdc.gov/obesity/data/maps/2022/2022-overall-prevalence.csv")  
